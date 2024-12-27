375ba5d02e98da5d661b998036fe12c01d07b1bd
Elvy1999 <elvydecoo99@gmail.com>
2024-12-26 21:34:26 -0500

    added the winning game functionality

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const buttons = document.querySelectorAll(".buttons");
+const score1 = document.getElementById("score1");
+const score2 = document.getElementById("score2");
+const result = document.getElementById("result");
+
+let humanScore = 0;
+let computerScore = 0;
+
 let getComputerChoice = () => {
   const randomNum = Math.floor(Math.random() * 3) + 1;
   if (randomNum == 1) return "rock";
@@ -5,84 +13,51 @@ let getComputerChoice = () => {
   if (randomNum == 3) return "paper";
 };
 
-let getHumanChoice = () => {
-  console.log("Select your weapon");
-  console.log("1 - rock");
-  console.log("2 - scissors");
-  console.log("3 - paper");
-  let userInput = prompt("Enter a selection");
-  if (userInput == 1) return "rock";
-  if (userInput == 2) return "scissors";
-  if (userInput == 3) return "paper";
-};
-
-let humanScore = 0;
-let computerScore = 0;
-
 let playRound = (computerChoice, userChoice) => {
-  userChoice = userChoice.toLowerCase();
   if (userChoice == computerChoice) {
     console.log("It's a tie!");
+    result.innerHTML = "It's a tie!";
   }
   if (userChoice == "rock" && computerChoice == "scissor") {
     humanScore++;
     console.log(" You win, rock beats scissors");
+    result.innerHTML = "You win, rock beats scissors";
   }
   if (userChoice == "rock" && computerChoice == "paper") {
-    computerChoice++;
+    computerScore++;
     console.log(" You lose, rock loses to paper");
+    result.innerHTML = " You lose, rock loses to paper";
   }
   if (userChoice == "scissors" && computerChoice == "rock") {
-    computerChoice++;
+    computerScore++;
     console.log(" You lose, scissors loses to rock");
+    result.innerHTML = " You lose, scissors loses to rock";
   }
   if (userChoice == "scissors" && computerChoice == "paper") {
     humanScore++;
     console.log(" You win, scissors beats paper");
+    result.innerHTML = " You win, scissors beats paper";
   }
   if (userChoice == "paper" && computerChoice == "scissors") {
     computerScore++;
     console.log(" You lose, paper loses to scissors");
+    result.innerHTML = " You lose, paper loses to scissors";
   }
   if (userChoice == "paper" && computerChoice == "rock") {
     humanScore++;
     console.log(" You win, paper beats rock");
+    result.innerHTML = "You win, paper beats rock";
   }
+
+  score1.innerHTML = humanScore;
+  score2.innerHTML = computerScore;
 };
 
-let playGame = () => {
-  while (humanScore < 5 && computerScore < 5) {
-    playRound(getComputerChoice(), getHumanChoice());
-  }
-  if (humanScore == 5) console.log("You have won 5 games");
-  else console.log("The computer has one 5 games");
-};
-
-const container = document.querySelector("#container");
-const pTag = document.createElement("p");
-pTag.innerHTML = "I am red";
-pTag.style.color = "red";
-container.appendChild(pTag);
-const h3 = document.createElement("h3");
-h3.innerHTML = "I'm a blue h3";
-h3.style.color = "blue";
-container.appendChild(h3);
-const div = document.createElement("div");
-div.style.border = "black 2px solid";
-div.style.background = "pink";
-container.appendChild(div);
-const nestedH1 = document.createElement("h1");
-nestedH1.innerHTML = "I'm in a div";
-const nestedP = document.createElement("p");
-nestedP.innerHTML = "ME TOO";
-div.appendChild(nestedH1);
-div.appendChild(nestedP);
-const button = document.createElement("button");
-button.style.border = "red 4px solid";
-button.innerHTML = "Click me";
-container.appendChild(button);
-button.id = "btn";
-let btn = document.getElementById("btn");
-btn.addEventListener("click", () => {
-  console.log("I have been clicked");
+buttons.forEach((button) => {
+  addEventListener("click", (e) => {
+    let userChoice = e.target.id;
+    playRound(getComputerChoice(), userChoice);
+    if (humanScore == 5) result.innerHTML = "Player has one a total of 5 games!";
+    else if (computerScore == 5) result.innerHTML = "Computer has one a total of 5 games!";
+  });
 });
